803840e1cc9b1c831fe45cca31f8ac9d8b76e550
hyun <dhtmdgussksm@naver.com>
2021-11-21 20:21:26 +0900

    fix z index to show side bar top
    same as title

--- injectHTML.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    document.getElementById("injectHTMLtarget").innerHTML='<object type="text/html" data="/index.html"></object>';
-})
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
 
   // Configuration
-  var menuWidth = 200; // Width of your menu
+  var menuWidth = 300; // Width of your menu
 
   var currentPageUrl = window.location.href;
   var stylesPath = chrome.runtime.getURL('styles.css');
@@ -13,22 +13,21 @@ setTimeout(function() {
    * HTML 삽입 여부 확인
    * 특정 키워드 기준(삽입 HTML DOM class) 존재할 경우 삽입 함수 실행 안함
    */
-  $(document).ready(function(){
-    document.getElementById("injectHTMLtarget").innerHTML='<object type="text/html" data="index.html"></object>';
+   $(document).ready(function(){
+     $('#injectHTMLtarget').html('<table"><tr><td>123</td><td>135</td></tr></table>');
   })
-  var injectPageHTML = '<div id="injectHTMLtarget"><table><tr><td>123</td></tr></table></div>';
+  var injectPageHTML = '<div id="injectHTMLtarget" style="width:100%; height:100%"></div>';
   if(currentPageSource.includes('injectedITXExtension')){
-    $('#injectedITXExtension').remove();
+    return 0;
   }else{
     $('head').html(
       currentPageHeadSource + 
-      '<script src="injectHTML.js"></script>' +
       '<link rel="stylesheet" href="'+stylesPath+'">'
     );
     $('body').html(
       '<div style="float:left; width: 100%; height: 100%;">' +
       '<div style="height: 100%; width: calc(100% - ' + menuWidth + 'px);">'+ currentPageBodySource + '</div>' + 
-      '<div id="injectedITXExtension" style="position: fixed; bottom: 0; right: 0; height: 100%; width:'+menuWidth+'px;">'+ injectPageHTML +'</div>' +
+      '<div id="injectedITXExtension" style="z-index:2147483647; position: fixed; bottom: 0; right: 0; height: 100%; width:'+menuWidth+'px;">'+ injectPageHTML +'</div>' +
       '</div>'
     );
   }
